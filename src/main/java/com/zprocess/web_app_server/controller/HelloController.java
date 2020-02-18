82cd3b89e7a36ab3fe5aad77e817f012608edcaa
ZProcess <jingcheng_zhang66@163.com>
2020-02-18 21:55:12 +0800

    springboot init

---
+++ src/main/java/com/zprocess/web_app_server/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.zprocess.web_app_server.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return "hello ZProcess";
+    }
+}
